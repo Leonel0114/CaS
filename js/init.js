94b2b744dae75c534b8734ff9dcda1f4850f4f85
Leonel <leonel_elizarraraz14@hotmail.com>
2019-03-18 15:57:33 -0600

    CaS
    Saturnospage

---
+++ js/init.js
@@ -0,0 +1,10 @@
+/* Esto hace funcionar el home */
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.materialboxed').materialbox();
+
+  });
+})(jQuery); 
